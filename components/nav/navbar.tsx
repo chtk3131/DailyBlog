1539b1e7eefe08d1943f28905ccb4c121f6d8f0e
落合拓也 <ochiaitakuya@testipa.local>
2024-05-01 17:48:44 +0900

    prettierを全体に適用させた。

--- components/nav/navbar.tsx
+++ components/nav/navbar.tsx
@@ -1,19 +1,21 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
 
 export default function Navbar() {
   return (
     <nav>
-      <div className='group'>
+      <div className="group">
         <Link href="/">DailyBlog</Link>
-        <div className='
+        <div
+          className="
           h-1
           w-0
           group-hover:w-full
           transition-all
           bg-green-500
-        '></div>
+        "
+        ></div>
       </div>
     </nav>
-  )
+  );
 }
