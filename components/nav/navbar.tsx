a9428cf4ae3dae17cfcc830a3b01765ea380f6e0
落合拓也 <ochiaitakuya@testipa.local>
2024-05-01 19:16:24 +0900

    react-iconsパッケージをインストール。

--- components/nav/navbar.tsx
+++ components/nav/navbar.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import React from 'react';
 import { Button } from '../ui/button';
+import { SiGithub } from 'react-icons/si';
 
 export default function Navbar() {
   return (
@@ -19,7 +20,10 @@ export default function Navbar() {
         "
         ></div>
       </div>
-      <Button variant="outline">Login</Button>
+      <Button variant="outline" className="flex items-center gap-2">
+        <SiGithub />
+        Login
+      </Button>
     </nav>
   );
 }
